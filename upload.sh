d52decab19866e30ac26cbae18f4159bcaa24b32
mzcorleone <mzcorleone@outlook.com>
2022-05-09 16:03:13 +0800

    update nodes

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 
 echo ""
 echo "订阅 https://raw.githubusercontent.com/mzcorleone/clash/main/fhsm.yaml"
-echo "节点 https://paste.gg/p/aweikeji/b0bb7818ded7462f9b6504281f487d16/files/8f4d3c492dc54683a53e14a05cddc3af/raw"
+echo "节点 https://raw.githubusercontent.com/mzcorleone/clash/main/node-all.yaml"
 echo ""
 
 git add *
