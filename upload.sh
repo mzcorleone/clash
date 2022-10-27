9ddb726dfd8216240cdd8f58df29bb2209064cc8
mzcorleone <mzcorleone@outlook.com>
2022-10-27 21:44:46 +0800

    update nodes

--- upload.sh
+++ upload.sh
@@ -19,4 +19,10 @@ git push
 # echo "Push to GitEE:15623586833:maga2024"
 # git push origin2
 
+echo "https://xianggelila.site/fhsm.yaml"
+echo "https://xianggelila.site/node-all.yaml"
+echo "administrator:Mfzk418X1h0t@120.25.254.81"
+scp *.yaml administrator@120.25.254.81:/C:/Users/Administrator/WWW/
+
+echo "END..."
 exit
