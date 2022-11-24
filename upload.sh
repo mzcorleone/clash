4e8245d98f226261d330fa25a590a12460b2a1d7
mzcorleone <mzcorleone@outlook.com>
2022-11-24 18:31:59 +0800

    update nodes

--- upload.sh
+++ upload.sh
@@ -19,15 +19,15 @@ git push
 # echo "Push to GitEE:15623586833:maga2024"
 # git push origin2
 
-echo ""
-echo ""
-echo "Uploading to iZEEKER"
-echo "https://xianggelila.site/fhsm.yaml"
-echo "https://xianggelila.site/node-all.yaml"
-echo "administrator:Mfzk418X1h0t@120.25.254.81"
-echo ""
-scp *.yaml administrator@120.25.254.81:/C:/Users/Administrator/WWW/
-echo ""
+# echo ""
+# echo ""
+# echo "Uploading to iZEEKER"
+# echo "https://xianggelila.site/fhsm.yaml"
+# echo "https://xianggelila.site/node-all.yaml"
+# echo "administrator:Mfzk418X1h0t@120.25.254.81"
+# echo ""
+# scp *.yaml administrator@120.25.254.81:/C:/Users/Administrator/WWW/
+# echo ""
 
 echo "END..."
 exit
